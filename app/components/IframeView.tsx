44b7f7455d0e3ef150f15ed7c6ab5ef7d2f38da6
robsoncoutinho1977 <rob.coutinho@gmail.com>
2025-12-09 20:16:21 -0300

    ajuste login webrtc

--- app/components/IframeView.tsx
+++ app/components/IframeView.tsx
@@ -9,8 +9,8 @@ export default function IframeView() {
     <div className="rounded-xl shadow-sm relative">
       <iframe
         ref={iframeRef}
-        //src="https://06.attimotelecom.com.br/webrtc"
-        src="https://06.attimotelecom.com.br"
+        src="https://06.attimotelecom.com.br/webrtc"
+        //src="https://06.attimotelecom.com.br"
         className="w-full h-[calc(100vh-8rem)] rounded-xl shadow-sm"
         title="Attimo POC"
         allow="microphone; camera"
